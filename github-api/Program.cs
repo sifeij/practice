5ab3142c864d18b284ce9cbde37aa6c0a8750c14
Sophie Jiang <sifeij@gmail.com>
2016-07-05 13:38:46 -0700

    Add more members to repo class and convert Json DateTime to .net DateTime

--- github-api/Program.cs
+++ github-api/Program.cs
@@ -16,6 +16,12 @@ namespace WebAPIClient
             foreach (var repo in repositories)
             {
                 Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine(repo.Homepage);
+                Console.WriteLine(repo.Watchers);
+                Console.WriteLine(repo.LastPush);
+                Console.WriteLine();
             }
         }
 
--- github-api/repo.cs
+++ github-api/repo.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Globalization;
 using System.Runtime.Serialization;
 
 namespace WebAPIClient
@@ -6,12 +8,37 @@ namespace WebAPIClient
     public class Repository
     {
         [DataMember(Name="name")]
-        private string _name;
         public string Name
         {
             get { return _name;}
             set { this._name = value;}
         }
-        
+
+        [DataMember(Name="description")]
+        public string Description { get; set; }
+
+        [DataMember(Name="html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [DataMember(Name="homepage")]
+        public Uri Homepage { get; set; }
+
+        [DataMember(Name="watchers")]
+        public int Watchers { get; set; }
+
+        [IgnoreDataMember]
+        public DateTime LastPush
+        {
+            get
+            {
+                return DateTime.ParseExact(
+                            JsonDate, "yyyy-MM-ddTHH:mm:ssZ", 
+                            CultureInfo.InvariantCulture);
+            }
+        }
+
+        [DataMember(Name="pushed_at")]
+        private string JsonDate { get; set; }
+        private string _name;
     }
 }

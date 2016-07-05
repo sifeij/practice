e171333f6c5fff4c4f6aea4678138cf84fd9a00f
Sophie Jiang <sifeij@gmail.com>
2016-07-05 13:01:51 -0700

    Create first version of github api project

---
+++ github-api/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ProcessRepositories().Wait();
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders
+                  .Accept.Add(new MediaTypeWithQualityHeaderValue(
+                                    "application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders
+                  .Add("User-Agent", ".NET Foundation Repository Reporter");
+            
+            var stringTask = client.GetStringAsync(
+                                    "https://api.github.com/orgs/dotnet/repos");
+            var msg = await stringTask;
+            Console.WriteLine(msg);
+
+        }
+    }
+}

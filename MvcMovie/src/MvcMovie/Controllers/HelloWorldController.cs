ab32501ecee3cd65ac285980dff75eae6270047b
Sophie Jiang <sifeij@gmail.com>
2016-07-07 07:30:52 -0700

    Add views. Use "ViewData" to display data on the view.

--- MvcMovie/src/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/src/MvcMovie/Controllers/HelloWorldController.cs
@@ -5,16 +5,30 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
-        // GET: /HelloWorld/Welcome/
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, id: {ID}"); //to protect the app from malicious input (namely JavaScript)
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
+
+        //// GET: /HelloWorld/Welcome/
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, id: {ID}"); //to protect the app from malicious input (namely JavaScript)
+        //}
+
+
+        //// GET: /HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
     }
 }

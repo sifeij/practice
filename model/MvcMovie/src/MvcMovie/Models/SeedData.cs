da4849a4fb7317ae3d95ff62a17868a8042ae10b
Sophie Jiang <sifeij@gmail.com>
2016-07-07 12:54:55 -0700

    Add a new field - Rating

--- model/MvcMovie/src/MvcMovie/Models/SeedData.cs
+++ model/MvcMovie/src/MvcMovie/Models/SeedData.cs
@@ -25,6 +25,7 @@ namespace MvcMovie.Models
                          Title = "When Harry Met Sally",
                          ReleaseDate = DateTime.Parse("1989-1-11"),
                          Genre = "Romantic Comedy",
+                         Rating = "R",
                          Price = 7.99M
                      },
                      new Movie
@@ -32,6 +33,7 @@ namespace MvcMovie.Models
                          Title = "Ghostbusters ",
                          ReleaseDate = DateTime.Parse("1984-3-13"),
                          Genre = "Comedy",
+                         Rating = "R",
                          Price = 8.99M
                      },
                      new Movie
@@ -39,6 +41,7 @@ namespace MvcMovie.Models
                          Title = "Ghostbusters 2",
                          ReleaseDate = DateTime.Parse("1986-2-23"),
                          Genre = "Comedy",
+                         Rating = "R",
                          Price = 9.99M
                      },
                    new Movie
@@ -46,6 +49,7 @@ namespace MvcMovie.Models
                        Title = "Rio Bravo",
                        ReleaseDate = DateTime.Parse("1959-4-15"),
                        Genre = "Western",
+                       Rating = "R",
                        Price = 3.99M
                    }
                 );

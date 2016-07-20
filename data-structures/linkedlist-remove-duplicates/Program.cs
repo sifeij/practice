95a5218071b0d2413a0c50191a6250b41baafa22
sifei jiang <sifeij@gmail.com>
2016-07-20 05:48:04 -0700

    Remove duplicates from simple linked list

--- data-structures/linkedlist-remove-duplicates/Program.cs
+++ data-structures/linkedlist-remove-duplicates/Program.cs
@@ -4,9 +4,88 @@ namespace ConsoleApplication
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(String[] args) {
+        
+            Node head=null;
+            int T = Int32.Parse(Console.ReadLine());
+            while(T-- > 0) {
+                int data = Int32.Parse(Console.ReadLine());
+                head = insert(head,data);
+            }
+            head = removeDuplicates(head);
+            display(head);
+        }
+        
+        public static Node removeDuplicates(Node head) {
+            //Write your code here
+            if(head == null || head.next == null) {
+                return head;
+            }
+            else {
+                var current = head;
+                
+                while (current.next != null) {
+                    if (current.data == current.next.data) {
+                        current.next = current.next.next;
+                    } else {
+                        current = current.next;
+                    }
+                }
+                return head;
+            }
+        }
+        
+        public static  Node insert(Node head,int data)
         {
-            Console.WriteLine("Hello World!!!!!!!!!!!!!!!");
+            Node p = new Node(data);
+            
+            if(head == null) {
+                head=p;
+            }
+            else if(head.next == null) {
+                head.next=p;
+            }
+            else {
+                Node start=head;
+                while(start.next != null)
+                    start = start.next;
+                start.next = p;
+            }
+            return head;
+        }
+        
+        public static void display(Node head)
+        {
+            Node start = head;
+            while(start != null)
+            {
+                Console.Write($"{start.data} ");
+                start = start.next;
+            }
+        }
+    }
+    
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d) {
+            data = d;
+            next = null;
         }
     }
 }
+
+/* input
+6
+1
+2
+2
+3
+3
+4
+*/
+
+/* output
+1 2 3 4
+*/

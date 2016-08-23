a4ab36cafdc9f73b4c572226de796d61ac297b28
sifei jiang <sifeij@gmail.com>
2016-08-23 10:10:10 -0700

    Use MaxBy extension method to get longest book

--- linq/linq-more-effective/Program.cs
+++ linq/linq-more-effective/Program.cs
@@ -10,72 +10,76 @@ namespace MoreEffectiveLinq
     {
         public static void Main(string[] args)
         {
-            WriteLine("*************** Range Expansion *************************************");
-            RangeExpansion();
+            // WriteLine("*************** Range Expansion *************************************");
+            // RangeExpansion();
 
-            WriteLine("*************** Sort By Age *************************************");
-            SortByAge();
+            // WriteLine("*************** Sort By Age *************************************");
+            // SortByAge();
 
             // WriteLine("*************** List Bishop Moves (dynamic) *************************************");
             // ListBishopMovesDynamic();
             // WriteLine("*************** List Bishop Moves (refactor) *************************************");
             // ListBishopMovesRefactor();
-            WriteLine("*************** List Bishop Moves (query) *************************************");
-            ListBishopMovesQuery();
+            // WriteLine("*************** List Bishop Moves (query) *************************************");
+            // ListBishopMovesQuery();
 
-            WriteLine("*************** Get Longest Book (aggregate) *************************************");
-            GetLongestBookUseAggregate();
+            // WriteLine("*************** Get Longest Book (aggregate) *************************************");
+            // GetLongestBookUseAggregate();
+            WriteLine("*************** Get Longest Book (MaxBy extension) *************************************");
+            GetLongestBookUseMaxByExtension();
 
-            WriteLine("*************** Get Album Duration (extension) *************************************");
-            GetAlbumDuration();
+            // WriteLine("*************** Get Album Duration (extension) *************************************");
+            // GetAlbumDuration();
         }
 
-        static void RangeExpansion()
+        static void GetAlbumDuration()
         {
-            // e.g. "2,3-5,7" should be 2,3,4,5,7
-            // e.g. "6,1-3,2-4" should be 1,2,3,4,6
-            var origin = "6,1-3,2-4,36-41";
-            var result = origin
+            var result = "2:54,3:48,4:51,3:32,6:15,4:08,5:17,3:13,4:16,3:55,4:53,5:35,4:24"
                 .Split(',')
-                .Select(x => x.Split('-'))
-                .Select(p => new { 
-                            First = int.Parse(p[0]), 
-                            Last = int.Parse(p.Last())})
-                .SelectMany(r => Enumerable.Range(
-                   r.First, r.Last - r.First + 1))
-                .OrderBy(r => r)
-                .Distinct()
-                .Select(n => n.ToString())
-                //.Aggregate((curr,next) => curr + "," + next)
-		        .Concat(",");
-                
+                .Select(t => "0:" + t)
+                .Select(t => TimeSpan.Parse(t))
+                //.Aggregate((t1,t2) => t1 + t2)
+                .Sum(); // using linq extension method
             WriteLine(result);
         }
 
-        static void SortByAge()
+        static void GetLongestBookUseMaxByExtension()
         {
-            var result = "Jason Puncheon, 26/06/1986; Jos Hooiveld, 22/04/1983; Kelvin Davis, 29/09/1976; Luke Shaw, 12/07/1995; Gaston Ramirez, 02/12/1990; Adam Lallana, 10/05/1988"
-                .Split(';')
-                .Select(n => n.Split(','))
-                .Select(n => new { Name = n[0].Trim(), DateOfBirth = ParseDob(n[1])})
-                .OrderByDescending(n => n.DateOfBirth)
-                .Select(n => new { Name = n.Name, Age = GetAge(n.DateOfBirth) });
-            
-            foreach(var item in result)
-            {
-                WriteLine($"{item.Name, -20}: {item.Age}");
-            }
-                
+            var books = new[] {
+                    new { Author = "Robert Martin", Title = "Clean Code", Pages = 464 },
+                    new { Author = "Oliver Sturm", Title = "Functional Programming in C#" , Pages = 270 },
+                    new { Author = "Martin Fowler", Title = "Patterns of Enterprise Application Architecture", Pages = 533 },
+                    new { Author = "Bill Wagner", Title = "Effective C#", Pages = 328 },
+                    };
+                    
+            var result = books.MaxBy(b => b.Pages); // best, but need to create the method
+
+            WriteLine(result);
         }
 
-        static int GetAge(DateTime dateOfBirth)
+        static void GetLongestBookUseAggregate()
         {
-            var today = DateTime.Today;
-            int age = today.Year - dateOfBirth.Year;
-            if (dateOfBirth > today.AddYears(-age)) age--;
-            return age;
-        }
+            var books = new[] {
+                    new { Author = "Robert Martin", Title = "Clean Code", Pages = 464 },
+                    new { Author = "Oliver Sturm", Title = "Functional Programming in C#" , Pages = 270 },
+                    new { Author = "Martin Fowler", Title = "Patterns of Enterprise Application Architecture", Pages = 533 },
+                    new { Author = "Bill Wagner", Title = "Effective C#", Pages = 328 },
+                    };
 
+            // books.First(b => b.Pages == books.Max(x => x.Pages)); // bad for performance
+
+            // var mostPages = books.Max(x => x.Pages);
+            // books.First(b => b.Pages == mostPages); // better for performance than above
+
+            // books.OrderByDescending(b => b.Pages).First(); // hmmm
+
+            var result = books.Aggregate((agg, next) => next.Pages > agg.Pages ? next : agg); // good but hard to read and understand
+
+            // books.MaxBy(b => b.Pages); // best, but need to create the method
+
+            WriteLine(result);
+        }
+        
         static DateTime ParseDob(string date) => DateTime.ParseExact(date.Trim(), "d/M/yyyy", CultureInfo.InvariantCulture);
     
         static void ListBishopMovesDynamic()
@@ -146,37 +150,48 @@ namespace MoreEffectiveLinq
             return dx == dy && dx != 0;
         }
 
-        static void GetLongestBookUseAggregate()
+        static void SortByAge()
         {
-            var books = new[] {
-                    new { Author = "Robert Martin", Title = "Clean Code", Pages = 464 },
-                    new { Author = "Oliver Sturm", Title = "Functional Programming in C#" , Pages = 270 },
-                    new { Author = "Martin Fowler", Title = "Patterns of Enterprise Application Architecture", Pages = 533 },
-                    new { Author = "Bill Wagner", Title = "Effective C#", Pages = 328 },
-                    };
-
-            // books.First(b => b.Pages == books.Max(x => x.Pages)); // bad for performance
-
-            // var mostPages = books.Max(x => x.Pages);
-            // books.First(b => b.Pages == mostPages); // better for performance than above
-
-            // books.OrderByDescending(b => b.Pages).First(); // hmmm
-
-            var result = books.Aggregate((agg, next) => next.Pages > agg.Pages ? next : agg); // good but hard to read and understand
-
-            // books.MaxBy(b => b.Pages); // best, but need to create the method
-
-            WriteLine(result);
+            var result = "Jason Puncheon, 26/06/1986; Jos Hooiveld, 22/04/1983; Kelvin Davis, 29/09/1976; Luke Shaw, 12/07/1995; Gaston Ramirez, 02/12/1990; Adam Lallana, 10/05/1988"
+                .Split(';')
+                .Select(n => n.Split(','))
+                .Select(n => new { Name = n[0].Trim(), DateOfBirth = ParseDob(n[1])})
+                .OrderByDescending(n => n.DateOfBirth)
+                .Select(n => new { Name = n.Name, Age = GetAge(n.DateOfBirth) });
+            
+            foreach(var item in result)
+            {
+                WriteLine($"{item.Name, -20}: {item.Age}");
+            }
         }
 
-        static void GetAlbumDuration()
+        static int GetAge(DateTime dateOfBirth)
         {
-            var result = "2:54,3:48,4:51,3:32,6:15,4:08,5:17,3:13,4:16,3:55,4:53,5:35,4:24"
+            var today = DateTime.Today;
+            int age = today.Year - dateOfBirth.Year;
+            if (dateOfBirth > today.AddYears(-age)) age--;
+            return age;
+        }
+
+        static void RangeExpansion()
+        {
+            // e.g. "2,3-5,7" should be 2,3,4,5,7
+            // e.g. "6,1-3,2-4" should be 1,2,3,4,6
+            var origin = "6,1-3,2-4,36-41";
+            var result = origin
                 .Split(',')
-                .Select(t => "0:" + t)
-                .Select(t => TimeSpan.Parse(t))
-                //.Aggregate((t1,t2) => t1 + t2)
-                .Sum(); // using linq extension method
+                .Select(x => x.Split('-'))
+                .Select(p => new { 
+                            First = int.Parse(p[0]), 
+                            Last = int.Parse(p.Last())})
+                .SelectMany(r => Enumerable.Range(
+                   r.First, r.Last - r.First + 1))
+                .OrderBy(r => r)
+                .Distinct()
+                .Select(n => n.ToString())
+                //.Aggregate((curr,next) => curr + "," + next)
+		        .Concat(",");
+                
             WriteLine(result);
         }
     }
--- linq/linq-more-effective/mylinq-extensions.cs
+++ linq/linq-more-effective/mylinq-extensions.cs
@@ -19,5 +19,31 @@ namespace MoreEffectiveLinq
         {
             return string.Join(separator, strings);
         }
+
+        public static TSource MaxBy<TSource, TKey>(this IEnumerable<TSource> source,
+		Func<TSource, TKey> selector)
+        {
+            var comparer = Comparer<TKey>.Default;
+            using (var sourceIterator = source.GetEnumerator())
+            {
+                if (!sourceIterator.MoveNext())
+                {
+                    throw new InvalidOperationException("Sequence contains no elements");
+                }
+                var max = sourceIterator.Current;
+                var maxKey = selector(max);
+                while (sourceIterator.MoveNext())
+                {
+                    var candidate = sourceIterator.Current;
+                    var candidateProjected = selector(candidate);
+                    if (comparer.Compare(candidateProjected, maxKey) > 0)
+                    {
+                        max = candidate;
+                        maxKey = candidateProjected;
+                    }
+                }
+                return max;
+            }
+        }
     }
 }

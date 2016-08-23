b903514777f2f633bfd375f4b709347c1e2ab1b8
sifei jiang <sifeij@gmail.com>
2016-08-22 18:59:31 -0700

    Get longest book using aggreate

--- linq/linq-more-effective/Program.cs
+++ linq/linq-more-effective/Program.cs
@@ -12,14 +12,19 @@ namespace ConsoleApplication
         {
             WriteLine("*************** Range Expansion *************************************");
             RangeExpansion();
+
             WriteLine("*************** Sort By Age *************************************");
             SortByAge();
-            WriteLine("*************** List Bishop Moves (dynamic) *************************************");
-            ListBishopMovesDynamic();
-            WriteLine("*************** List Bishop Moves (refactor) *************************************");
-            ListBishopMovesRefactor();
+
+            // WriteLine("*************** List Bishop Moves (dynamic) *************************************");
+            // ListBishopMovesDynamic();
+            // WriteLine("*************** List Bishop Moves (refactor) *************************************");
+            // ListBishopMovesRefactor();
             WriteLine("*************** List Bishop Moves (query) *************************************");
             ListBishopMovesQuery();
+
+            WriteLine("*************** Get Longest Book (aggregate) *************************************");
+            GetLongestBookUseAggregate();
         }
 
         static void RangeExpansion()
@@ -136,6 +141,29 @@ namespace ConsoleApplication
             var dx = Math.Abs(startPos[0] - targetPos[0]);
             var dy = Math.Abs(startPos[1] - targetPos[1]);
             return dx == dy && dx != 0;
+        }
+
+        static void GetLongestBookUseAggregate()
+        {
+            var books = new[] {
+                    new { Author = "Robert Martin", Title = "Clean Code", Pages = 464 },
+                    new { Author = "Oliver Sturm", Title = "Functional Programming in C#" , Pages = 270 },
+                    new { Author = "Martin Fowler", Title = "Patterns of Enterprise Application Architecture", Pages = 533 },
+                    new { Author = "Bill Wagner", Title = "Effective C#", Pages = 328 },
+                    };
+
+            // books.First(b => b.Pages == books.Max(x => x.Pages)); // bad for performance
+
+            // var mostPages = books.Max(x => x.Pages);
+            // books.First(b => b.Pages == mostPages); // better for performance than above
+
+            // books.OrderByDescending(b => b.Pages).First(); // hmmm
+
+            var result = books.Aggregate((agg, next) => next.Pages > agg.Pages ? next : agg); // good but hard to read and understand
+
+            // books.MaxBy(b => b.Pages); // best, but need to create the method
+
+            WriteLine(result);
         }
     }
 }
